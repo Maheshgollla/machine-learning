1897a9b3233d5af06bc59bd6e4f2d6ea0479d4fa
Maheshgollla <mahesh.golla072@gmail.com>
2025-02-10 11:31:17 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-if __name__=="__main__":
-    x=[]
-    n=int(input('enter array size: '))
-    for i in range(n):
-        a=int(input(f"Enter element number {i}: "))
-        x.append(a)
-    print(x)
